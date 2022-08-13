eaa212f4a7e99e5618d319c7120dd6b946fd96f9
aditnag <iamaditnag034@gmail.com>
2022-08-13 22:59:18 +0530

    view all employees

--- emp_app/urls.py
+++ emp_app/urls.py
@@ -4,4 +4,9 @@ from . import views
 app_name = 'emp_app'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp'),
+
 ]
