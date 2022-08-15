2014adbb21bf63c2a66e14e2484ce0bab7a10bb0
aditnag <iamaditnag034@gmail.com>
2022-08-15 13:35:05 +0530

    filter complete

--- emp_app/views.py
+++ emp_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from .models import Employee, Role, Department
 from datetime import datetime
 from django.urls import reverse
+from django.db.models import Q
 
 
 # Create your views here.
@@ -41,7 +42,7 @@ def add_emp(request):
 def remove_emp(request, emp_id=0):
     if emp_id:
         try:
-            emp_to_be_removed = Employee.objects.get(id = emp_id)
+            emp_to_be_removed = Employee.objects.get(id=emp_id)
             emp_to_be_removed.delete()
             return HttpResponse("Employee removed successfully!")
         except:
@@ -54,4 +55,24 @@ def remove_emp(request, emp_id=0):
 
 
 def filter_emp(request):
-    return render(request, 'emp_app/filter_emp.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        dept = request.POST['dept']
+        role = request.POST['role']
+        emps = Employee.objects.all()
+        if name:
+            emps = emps.filter(Q(first_name__icontains=name) | Q(last_name__icontains=name))
+        if dept:
+            emps = emps.filter(dept__name__icontains=dept)
+        if role:
+            emps = emps.filter(role__name__icontains=role)
+
+        context = {
+            'emps': emps
+        }
+        # return render(request, 'view_all_emp.html', context)
+        return HttpResponseRedirect(reverse('emp_app:all_emp'))
+    elif request.method == 'GET':
+        return render(request, 'emp_app/filter_emp.html')
+    else:
+        return HttpResponse("An Exception Occurred")
